05d55abd0d003f91540a4c49091f1942b6c463af
Daniel Korrel <dkorrel@gmail.com>
2022-01-13 13:35:53 +0100

    Added destroy behaviour

---
+++ Runtime/Scripts/Destroy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Korrel.VFXOnEventHandler
+{
+    public class Destroy : BaseEffectBehaviour
+    {
+       
+    }
+}
